ae066db9b314bb32aef9d62548f72dcc6e1bbe56
Emma Walker <walkere@tcd.ie>
2023-03-31 17:27:50 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 31/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
